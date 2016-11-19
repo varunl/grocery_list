2492ad2d35247ca9572f86b0f2f25b68964f9f0e
Varun Loiwal <varunl@users.noreply.github.com>
2016-11-19 14:01:43 -0800

    Add configurable port

--- server.go
+++ server.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 )
+
+var port = flag.String("port", "8080", "Port to start the webserver on")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!")
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	fmt.Printf("Starting webserver, listening on port %s...\n", *port)
+	http.ListenAndServe(":"+*port, nil)
 }
